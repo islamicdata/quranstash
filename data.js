9fec41d948a3ea89432b7043539e0aff44cbaa43
islamicdata <159684698+islamicdata@users.noreply.github.com>
2024-02-12 00:19:51 -0500

    Update data.js

--- data.js
+++ data.js
@@ -15,11 +15,18 @@ const quotesData = {
             "mp3": "2.mp3"
         },
         {
-            "verse-number": "(3:19)",
-            "arabic-text": "إِنَّ الدِّينَ عِندَ اللَّهِ الْإِسْلَامُ ۗ وَمَا اخْتَلَفَ الَّذِينَ أُوتُوا الْكِتَابَ إِلَّا مِن بَعْدِ مَا جَاءَهُمُ الْعِلْمُ بَغْيًا بَيْنَهُمْ ۗ وَمَن يَكْفُرْ بِآيَاتِ اللَّهِ فَإِنَّ اللَّهَ سَرِيعُ الْحِسَابِ",
-            "end": "۟",
-            "translation": "Indeed, the religion in the sight of Allah is Islam. And those who were given the Scripture did not differ except after knowledge had come to them - out of jealous animosity between themselves. And whoever disbelieves in the verses of Allah, then indeed, Allah is swift in [taking] account.",
-            "mp3": "1.mp3"
+            "verse-number": "(2:285)",
+            "arabic-text": "اٰمَنَ الرَّسُوْلُ بِمَاۤ اُنْزِلَ اِلَیْهِ مِنْ رَّبِّهٖ وَالْمُؤْمِنُوْنَ ؕ— كُلٌّ اٰمَنَ بِاللّٰهِ وَمَلٰٓىِٕكَتِهٖ وَكُتُبِهٖ وَرُسُلِهٖ ۫— لَا نُفَرِّقُ بَیْنَ اَحَدٍ مِّنْ رُّسُلِهٖ ۫— وَقَالُوْا سَمِعْنَا وَاَطَعْنَا غُفْرَانَكَ رَبَّنَا وَاِلَیْكَ الْمَصِیْرُ",
+            "end": "۟",
+            "translation": "The Messenger ˹firmly˺ believes in what has been revealed to him from his Lord, and so do the believers. They ˹all˺ believe in Allah, His angels, His Books, and His messengers. ˹They proclaim,˺ “We make no distinction between any of His messengers.” And they say, “We hear and obey. ˹We seek˺ Your forgiveness, our Lord! And to You ˹alone˺ is the final return.” [Second to Last Verse of Surah Al-Baqara]",
+            "mp3": "2.mp3"
+        },
+        {
+            "verse-number": "(2:286)",
+            "arabic-text": "لَا یُكَلِّفُ اللّٰهُ نَفْسًا اِلَّا وُسْعَهَا ؕ— لَهَا مَا كَسَبَتْ وَعَلَیْهَا مَا اكْتَسَبَتْ ؕ— رَبَّنَا لَا تُؤَاخِذْنَاۤ اِنْ نَّسِیْنَاۤ اَوْ اَخْطَاْنَا ۚ— رَبَّنَا وَلَا تَحْمِلْ عَلَیْنَاۤ اِصْرًا كَمَا حَمَلْتَهٗ عَلَی الَّذِیْنَ مِنْ قَبْلِنَا ۚ— رَبَّنَا وَلَا تُحَمِّلْنَا مَا لَا طَاقَةَ لَنَا بِهٖ ۚ— وَاعْفُ عَنَّا ۥ— وَاغْفِرْ لَنَا ۥ— وَارْحَمْنَا ۥ— اَنْتَ مَوْلٰىنَا فَانْصُرْنَا عَلَی الْقَوْمِ الْكٰفِرِیْنَ",
+            "end": "۟۠",
+            "translation": "Allah does not require of any soul more than what it can afford. All good will be for its own benefit, and all evil will be to its own loss. ˹The believers pray,˺ “Our Lord! Do not punish us if we forget or make a mistake. Our Lord! Do not place a burden on us like the one you placed on those before us. Our Lord! Do not burden us with what we cannot bear. Pardon us, forgive us, and have mercy on us. You are our ˹only˺ Guardian. So grant us victory over the disbelieving people.”[Last Verse of Surah Al-Baqara]",
+            "mp3": "2.mp3"
         }
         // Add more quotes here
     ]
